6d37f72ff829289ba2e07bb406d5689cd5b10ae8
alcar7606 <acardenas7606@gmail.com>
2023-01-05 13:04:15 -0600

    Personalizo index

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //Render Invoca al Templete Engine (HBS)
+  //Se le proporcionan 2 argumentos
+  //res.render(<vista>, <view-model>)
+  //res.render(plantilla, datos)
+  res.render('index', { 
+    title: 'ALVARO CARDENAS ',
+    welcomeMessage: 'Bienvenidos a mi sitio personal'
+  });
 });
 
 module.exports = router;

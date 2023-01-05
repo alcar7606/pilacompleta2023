8e66177ab03368b4eb1bbf072ee23dc3eea45f3f
alcar7606 <acardenas7606@gmail.com>
2023-01-05 11:02:48 -0600

    Se agrega API autor

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
+//Dependencias de la app
+//Esta permite manejar errores http
 var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//Esta es la dependencia principal
+//para crear el servidor Express.js
 
+var express = require('express');
+//Esta es del nucleo de Node
+//Permite manejar rutas del sistema de archivos
+var path = require('path');
+//Ayuda a manejar las cookies
+var cookieParser = require('cookie-parser');
+//Permite registrar (loggeux) en la consola
+//eventos de la aplicacion o server
+var logger = require('morgan');
+//Definen el controlador o acciones de respuesta
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+//Se crea la instancia de express
+//osea el server
 var app = express();
 
 // view engine setup
@@ -18,9 +29,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+//Registramos una ruta y que se hara como respuesta
+//Los argumentos son (<ruta:string>,<middleware:fn>)
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/author',(req,res)=>{
+  //respondiendo con un json
+  res.status(200).json({
+    "author" : "Alvaro Cárdenas"
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
